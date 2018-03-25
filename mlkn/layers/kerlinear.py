210a4d3578550aad2538e99a24a52fba7684a7d8
michaelshiyu <michaelshiyu3@gmail.com>
2018-03-25 13:32:17 -0400

    built kerLinear layer, forward and backward runs ok

--- mlkn/layers/kerlinear.py
+++ mlkn/layers/kerlinear.py
@@ -2,26 +2,99 @@
 
 import torch
 from torch.autograd import Variable
-from .. import backend as
+import torch_backend as K # TODO: relative import
 
 class kerLinear(torch.nn.Module):
-    def __init__(self, out_dim):
+    def __init__(self, ker_dim, out_dim, sigma, bias=True):
         """
         Building block for MLKN.
-        A kernel linear layer first applies to input a nonlinear map
-        determined by the kernel function and the given random sample, then
-        linearly map the image to a new representation in some Euclidean space
+        A kernel linear layer first applies to input sample x a nonlinear map
+        determined by the kernel function and the given random sample X:
+
+            f : x -> R^n
+            f : f(x_) -> (k(x_1, x_), k(x_2, x_), ..., k(x_n, x_)),
+
+        where x_ \in x, k is a kernel function and X = {x_1, x_2, ..., x_n}.
+        Then it linearly maps the image to some Euclidean space
         with dimension determined by the number of kernel machines in this
-        layer.
+        layer. Currently only supports Gaussian kernel:
+        k(x, y) = exp(-||x-y||_2^2 / (2 * sigma^2)). Currently only supports
+        1darrays or 2darrays as input.
 
         Parameters
         ----------
         out_dim : int
             The number of kernel machines in this layer.
-
-        Returns
-        -------
+        ker_dim : int
+            Dimension of the codomain of f, or, equivalently, the size of the
+            random sample X.
+        bias (optional) : bool
+            If True, add a bias term to the linear combination.
 
         Attributes
         ----------
         """
+        super(kerLinear, self).__init__()
+
+        self.sigma = sigma
+        self.ker_dim = ker_dim
+
+        self.kerMap = K.kerMap
+        self.linear = torch.nn.Linear(ker_dim, out_dim, bias=bias)
+        # TODO: customizable weight initializations
+
+    def forward(self, x, X):
+        """
+        Parameters
+        ----------
+
+        x : Tensor, shape (batch_size, dim)
+
+        X : Tensor, shape (n_example, dim)
+
+        Returns
+        -------
+        y : Tensor, shape (batch_size, out_dim)
+        """
+        if len(X.shape)==1: assert self.ker_dim==1 # does not modify the
+        # dimension of X here as this will be done later in self.kerMap
+        else: assert self.ker_dim==X.shape[0]
+
+        x_image = self.kerMap(x, X, self.sigma)
+
+        print('x_image', x_image)
+
+        y = self.linear(x_image)
+        return y
+
+if __name__=='__main__':
+
+    dtype = torch.FloatTensor
+    if torch.cuda.is_available():
+        dtype = torch.cuda.FloatTensor
+
+    x = Variable(torch.FloatTensor([[0, 7], [1, 2]]).type(dtype))
+    X = Variable(torch.FloatTensor([[1, 2], [3, 4], [5, 6]]).type(dtype))
+    y = Variable(torch.FloatTensor([[.3], [.9]]).type(dtype))
+
+    l = kerLinear(ker_dim=3, out_dim=1, sigma=1, bias=True)
+
+    y_pred = l(x=x, X=X)
+    print('y_pred', y_pred)
+    print('weight', l.linear.weight)
+    print('bias', l.linear.bias)
+    print('params', list(l.parameters()))
+
+    """
+    criterion = torch.nn.MSELoss(size_average=False)
+    optimizer = torch.optim.SGD(l.parameters(), lr=1e-4)
+    for t in range(500):
+        y_pred = l(x, X)
+        loss = criterion(y_pred, y)
+        print(t, loss.data[0])
+
+        # Zero gradients, perform a backward pass, and update the weights.
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+    """
---
+++ mlkn/layers/torch_backend.py
@@ -0,0 +1,76 @@
+# -*- coding: utf-8 -*-
+
+import torch
+import numpy as np
+
+def gaussianKer(x, y, sigma):
+    # tested under torch 0.3.1
+    """
+    Gaussian kernel: k(x, y) = exp(-||x-y||_2^2 / (2 * sigma^2)).
+    Arguments must be matrices or 1darrays. 1darrays will be converted to
+    matrices. While the last dimension of its two
+    arguments must be identical, this function supports broadcasting in the
+    first dimension. Can be used to calculate Gram matrix.
+
+    Parameters
+    ----------
+
+    x : Tensor, shape (n1_example, dim)
+
+    y : Tensor, shape (n2_example, dim)
+
+    sigma : scalar
+
+    Returns
+    -------
+
+    gram : Tensor, shape (n1_example, n2_example)
+        Technically not a Gram matrix when x!=y, only using this name for
+        convenience.
+    """
+    if len(x.shape)==1: x.unsqueeze_(0)
+    if len(y.shape)==1: y.unsqueeze_(0)
+    assert len(x.shape)==2 and len(y.shape)==2 and x.shape[1]==y.shape[1]
+    # TODO: if len(x.shape)>=2 but only two dimensions are nontrivial, should
+    # allow computation after squeezing into 2darray
+    # TODO: support ndarrays where n>2, e.g., RGB images may have shape
+    # (n_example, 3_channels, channel_dim). shape of return should
+    # always be (n1_example, n2_example), need to be careful with broadcasting
+    # and which dimensions to sum out in this part:
+    # y.sub(x.unsqueeze(1)).pow(2).sum(dim=-1)
+
+    gram = y.sub(x.unsqueeze(1)).pow(2).sum(dim=-1).mul(-1./(2*sigma**2)).exp()
+    return gram
+
+
+def kerMap(x, X, sigma):
+    """
+    For all x_ \in x, computes the image of x_ under the mapping:
+        f: f(x_) -> (k(x_1, x_), k(x_2, x_), ..., k(x_n, x_)),
+    where k is a kernel function and X = {x_1, x_2, ..., x_n}.
+    Currently only supports Gaussian kernel:
+    k(x, y) = exp(-||x-y||_2^2 / (2 * sigma^2)).
+
+    Parameters
+    ----------
+
+    x : Tensor, shape (batch_size, dim)
+
+    X : Tensor, shape (n_example, dim)
+
+    sigma : scalar
+
+    Returns
+    -------
+
+    x_image : Tensor, shape (batch_size, n_example)
+    """
+    x_image = gaussianKer(x, X, sigma)
+
+    return x_image
+
+if __name__=='__main__':
+    x = torch.FloatTensor([[1, 2]])
+    X = torch.FloatTensor([[1, 2], [3, 4], [5, 6]])
+    y = kerMap(x, X, sigma=1)
+    print(y)
