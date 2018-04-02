f8018f27699347f0e289365e75966462c8b87cae
michaelshiyu <michaelshiyu3@gmail.com>
2018-04-02 00:13:49 -0400

    uploaded first MWE and revised README

--- README.md
+++ README.md
@@ -6,6 +6,14 @@ For convenience, a few higher-level model abstractions are also available, inclu
 
 Building a network with kernel machines here is as straightforward as building a neural network in PyTorch: you basically just need to read [this great tutorial](http://pytorch.org/tutorials/beginner/blitz/neural_networks_tutorial.html#sphx-glr-beginner-blitz-neural-networks-tutorial-py).
 
+For now, the project is very new and fairly unstable. We have much to add to it and we are also working on optimizing what's already there. Major functionalities have been tested (on CPU) under the following setup:
+- Python 3
+- PyTorch 0.3.1
+- Numpy 1.14.1
+- Scikit-Learn 0.19.1
+
+Currently, the best way to work with this API is by forking it or via ```git clone```. Hope you enjoy it and any suggestion or contribution would be greatly appreciated!
+
 # MultiLayer Kernel Network
 
 A MLKN is an equivalent of a fully-connected, feedforward neural network in the universe of kernel machines. It works just like a [MultiLayer Perceptron (MLP)](https://en.wikipedia.org/wiki/Multilayer_perceptron) except that under the hood, everything is now being powered by [kernel machines](https://en.wikipedia.org/wiki/Radial_basis_function_network).
@@ -14,10 +22,10 @@ In this repository, you will find a pre-built yet still highly customizable MLKN
 
 For an example, see this tutorial.
 
-(add tutorial)
+(For a minimal working example, see examples/mlkn_classifier, where there is a classifier model exactly the same as the one implemented in the above paper and three datasets preprocessed in the same way as those used in the paper. A more detailed tutorial will be added soon.)
 
 # Lower-Level Kernel Machine-Based Objects
 
 Apart from the pre-built, high-level models, we offer some basic building blocks to give you more freedom to customize.
 
-(add tutorial)
+(Tutorials will be added soon.)
--- examples/mlkn_classification.py
+++ examples/mlkn_classification.py
@@ -18,11 +18,15 @@ from layers.kerlinear import kerLinear
 torch.manual_seed(1234)
 
 if __name__=='__main__':
-
-    x, y = load_breast_cancer(return_X_y=True)
-    # x, y = load_digits(return_X_y=True)
+    """
+    This example demonstrates how a MLKN classifier works. Everything in here
+    strictly follows this paper: https://arxiv.org/pdf/1802.03774.pdf.
+    """
+    # x, y = load_breast_cancer(return_X_y=True)
+    x, y = load_digits(return_X_y=True)
     # x, y = load_iris(return_X_y=True)
 
+    # standardize features to zero-mean and unit-variance
     normalizer = StandardScaler()
     x = normalizer.fit_transform(x)
     n_class = int(np.amax(y) + 1)
@@ -30,25 +34,37 @@ if __name__=='__main__':
     dtype = torch.FloatTensor
     if torch.cuda.is_available():
         dtype = torch.cuda.FloatTensor
-
     X = Variable(torch.from_numpy(x).type(dtype), requires_grad=False)
     Y = Variable(torch.from_numpy(y).type(dtype), requires_grad=False)
+
+    # randomly permute data
     new_index = torch.randperm(X.shape[0])
     X, Y = X[new_index], Y[new_index]
 
+    # split data evenly into training and test
     index = len(X)//2
     x_train, y_train = X[:index], Y[:index]
     x_test, y_test = X[index:], Y[index:]
 
     mlkn = MLKNClassifier()
-    mlkn.add_layer(kerLinear(ker_dim=x_train.shape[0], out_dim=15, sigma=5, bias=True))
-    mlkn.add_layer(kerLinear(ker_dim=x_train.shape[0], out_dim=n_class, sigma=.1, bias=True))
-
-    mlkn.add_optimizer(torch.optim.Adam(params=mlkn.parameters(), lr=1e-3, weight_decay=0.1))
-    mlkn.add_optimizer(torch.optim.Adam(params=mlkn.parameters(), lr=1e-3, weight_decay=.1))
-
+    # add layers to the model, see layers/kerlinear for details on kerLinear
+    mlkn.add_layer(
+        kerLinear(ker_dim=x_train.shape[0], out_dim=15, sigma=5, bias=True)
+        )
+    mlkn.add_layer(
+        kerLinear(ker_dim=x_train.shape[0], out_dim=n_class, sigma=.1, bias=True)
+        )
+    # add optimizer for each layer, this works with any torch.optim.Optimizer
+    mlkn.add_optimizer(
+        torch.optim.Adam(params=mlkn.parameters(), lr=1e-3, weight_decay=0.1)
+        )
+    mlkn.add_optimizer(
+        torch.optim.Adam(params=mlkn.parameters(), lr=1e-3, weight_decay=.1)
+        )
+    # specify loss function for the output layer, this works with any
+    # PyTorch loss function but it is recommended that you use CrossEntropyLoss
     mlkn.add_loss(torch.nn.CrossEntropyLoss())
-
+    # fit the model
     mlkn.fit(
         n_epoch=(30, 30),
         batch_size=30,
@@ -57,6 +73,7 @@ if __name__=='__main__':
         Y=y_train,
         n_class=n_class
         )
+    # make a prediction on the test set and print error
     y_pred = mlkn.predict(X_test=x_test, X=x_train, batch_size=15)
     err = mlkn.get_error(y_pred, y_test)
     print('error rate: {:.2f}%'.format(err.data[0] * 100))
